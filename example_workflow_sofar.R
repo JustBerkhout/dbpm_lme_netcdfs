706719ce1a7a23bc006e247f0e5668e0387613a0
Just Berkhout <just.berkhout@utas.edu.au>
2018-11-19 15:06:09 +1100

    rotate NetCDF data, fixes #1

--- example_workflow_sofar.R
+++ example_workflow_sofar.R
@@ -1,6 +1,7 @@
 library(raster)
 #load nc file into raster::brick object and plot first layer
 my_brick <- raster::brick("/rd/gem/private/GCM_INPUT/IPSL_CM5A_LR/rcp26/ipsl-cm5a-lr_rcp26_lphy_zint_monthly_200601_21001231.nc4")
+my_brick <- raster::rotate(my_brick) #change coords from 0 to 360 to -180 to 180
 plot(my_brick[[1]]) #plots the firstlayer of the world lphy brick
 
 
@@ -13,7 +14,7 @@ plot(LMEs, col=ElevenColours[LMEs$LME_NUMBER%%11], border="white", lwd=1)
 
 #pick a random LME and plot it
 iLME <- sample(nrow(LMEs), 1)
-iLME <- 26
+iLME <- 65
 selectedLME <- LMEs[LMEs$LME_NUMBER==iLME,]
 plot(selectedLME, lwd=1)
 
@@ -28,13 +29,4 @@ plot(extent(selectedLME), type="n", asp=1, xaxt='n', yaxt='n', ann=FALSE)
 plot(my_brick[[1]], col=colorspace::sequential_hcl(1), legend=FALSE, add=TRUE)
 plot(masked_brick[[1]], legend=FALSE, add=TRUE) #plots the first layer of the selectedLME cropped lphy brick
 plot(selectedLME, lwd=1, add = TRUE)
-
-
-
-#this logic needs vetting:
-# iLME = 13  (humboldt current, appears empty)
-# LME spanning dateline, break, I think
-# for some LMEs this error occurs:
-# > cropped_brick <- raster::crop(my_brick, selectedLME) 
-#  Error in .local(x, y, ...) : extents do not overlap
 
