3bb39ac74d866b8258ee05bc1e29e2140714112b
Just Berkhout <just.berkhout@utas.edu.au>
2018-11-23 22:06:31 +1100

    add month (yyyy-mm) to gcm_by_lme data

--- merge_gcm_by_lme.R
+++ merge_gcm_by_lme.R
@@ -1,5 +1,6 @@
 #merge files into one rds
 library(tidyverse)
+library(lubridate)
 
 my_working_path <- "/rd/gem/private/users/justb/gcm_lmescale_v2/"
 
@@ -14,6 +15,9 @@ df <- list.files(my_working_path, "^hist|^rcp", full.names = TRUE) %>%
   ) %>% 
   inner_join(lme_depths) %>% 
   select(-min, -max, -mean) %>% 
-  rename(aw_mean_depth = aw_mean)
+  rename(aw_mean_depth = aw_mean) %>% 
+  mutate(ts_abs = ifelse(scenario=="historical", ts, ts + 672)) %>% 
+  mutate(month = substr(as.character(ymd("1950-01-15") %m+% months(ts_abs-1 )), 1, 7)) %>% 
+  select(-ts_abs)
 
 saveRDS(df, paste0(my_working_path, "/lme_scale_gcm_inputs.rds"))
