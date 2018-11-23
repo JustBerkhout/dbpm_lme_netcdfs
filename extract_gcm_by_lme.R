d7c6fc2e12e6f74818ddab06fe8d64d27086113e
Just Berkhout <just.berkhout@utas.edu.au>
2018-11-23 14:26:34 +1100

    enable parallel running on 10 cores

--- extract_gcm_by_lme.R
+++ extract_gcm_by_lme.R
@@ -11,9 +11,6 @@
 # spatial average with cell size weight
 source("R/shared.R")
 
-my_working_path <- "/rd/gem/private/users/justb/gcm_lmescale_v2"
-
-
 .get_filename <- function(sce, var){
   #/rd/gem/private/GCM_INPUT/IPSL_CM5A_LR/historical/ipsl-cm5a-lr_historical_lphy_zint_monthly_195001_200512.nc4
   #/rd/gem/private/GCM_INPUT/IPSL_CM5A_LR/rcp26/ipsl-cm5a-lr_rcp26_lphy_zint_monthly_200601_21001231.nc4
@@ -65,43 +62,52 @@ my_working_path <- "/rd/gem/private/users/justb/gcm_lmescale_v2"
   return(df)
 }
 
-
-#load LME shape file
-lmes <- rgdal::readOGR(dsn ="/rd/gem/private/fishmip_inputs/misc/LMEs66/", layer = "LMEs66", verbose = FALSE)
-lme_numbers <- sort(lmes$LME_NUMBER)
-
-scenarios <- c("historical", "rcp26", "rcp45", "rcp60", "rcp85")
-variables <- c("lphy", "sphy", "sbt", "sst")
-
 #testing with reduced data
 # scenarios <- sample(scenarios, 1)
 # variables <- sample(variables, 1)
 
 
-for (scenario in scenarios){
-    for (variable in variables){
-      out_file_name <- sprintf("%s/%s_%s.rds", my_working_path, scenario, variable)
-      if(!file.exists(out_file_name)) {
-        list_out <- list()
-        i <- 1        
-        my_file <- .get_filename(scenario, variable)
-        my_brick <- raster::brick(my_file)
-        my_brick <- raster::rotate(my_brick) #change coords from 0 to 360 to -180 to 180
-        for (lme_number in lme_numbers) {
-          selected_lme <- lmes[lmes$LME_NUMBER==lme_number,]
-          res <- .cropmask(my_brick, selected_lme)
-          list_out[[i]]  <- .brick2df(res, scenario, variable, lme_number)
-          i <- i + 1
-          }#lme_number
-        df_out <- dplyr::bind_rows(list_out)
-        saveRDS(df_out, file=out_file_name)
-        rm(list_out)
-      }
-    } #variable
-} #scenario
+.worker <- function(sce, var, output_path){
+  #load LME shape file
+  lmes <- rgdal::readOGR(dsn ="/rd/gem/private/fishmip_inputs/misc/LMEs66/", layer = "LMEs66", verbose = FALSE)
+  lme_numbers <- sort(lmes$LME_NUMBER)  
+  out_file_name <- sprintf("%s/%s_%s.rds", output_path, sce, var)
+  if(!file.exists(out_file_name)) {
+    list_out <- list()
+    i <- 1        
+    my_file <- .get_filename(sce, var)
+    my_brick <- raster::brick(my_file)
+    my_brick <- raster::rotate(my_brick) #change coords from 0 to 360 to -180 to 180
+    for (lme_number in lme_numbers) {
+      selected_lme <- lmes[lmes$LME_NUMBER==lme_number,]
+      res <- .cropmask(my_brick, selected_lme)
+      list_out[[i]]  <- .brick2df(res, sce, var, lme_number)
+      i <- i + 1
+    }#lme_number
+    df_out <- dplyr::bind_rows(list_out)
+    saveRDS(df_out, file=out_file_name)
+    rm(list_out)
+  }
+  
+}
 
 
+main <- function(){
+    my_working_path <- "/rd/gem/private/users/justb/gcm_lmescale_v2"
+  
+    scenarios <- c("historical", "rcp26", "rcp45", "rcp60", "rcp85")
+    variables <- c("lphy", "sphy", "sbt", "sst")
+  
+    g <- expand.grid(scenarios=scenarios, variables=variables)
+    
+    cl <- parallel::makeCluster(10)
+    parallel::clusterExport(cl, c(".brick2df", ".cropmask", ".get_filename") )
+    results <- parallel::clusterMap(cl, .worker, sce=g$scenarios, var=g$variables, output_path=my_working_path )
+    
+    parallel::stopCluster(cl)
+}
 
+main()
 
 
 
