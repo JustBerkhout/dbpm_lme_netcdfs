198f283700e2407e69aaa92b204590cd7e1db916
Just Berkhout <just.berkhout@utas.edu.au>
2018-11-23 14:49:08 +1100

    add merge routine

---
+++ merge_gcm_by_lme.R
@@ -0,0 +1,19 @@
+#merge files into one rds
+library(tidyverse)
+
+my_working_path <- "/rd/gem/private/users/justb/gcm_lmescale_v2/"
+
+lme_depths <- readRDS(paste0(my_working_path, "/lme_depths.rds"))
+
+
+df <- list.files(my_working_path, "^hist|^rcp", full.names = TRUE) %>% 
+  map_dfr(readRDS) %>% 
+  mutate(
+    scenario = factor(scenario),
+    variable = factor(variable)
+  ) %>% 
+  inner_join(lme_depths) %>% 
+  select(-min, -max, -mean) %>% 
+  rename(aw_mean_depth = aw_mean)
+
+saveRDS(df, paste0(my_working_path, "/lme_scale_gcm_inputs.rds"))
